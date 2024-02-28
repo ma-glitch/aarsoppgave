66586adb6f7412b728905c55db0102a09adda8d8
Martin_T <Tangenmartin06@gmail.com>
2024-02-28 09:32:33 +0100

    dededed

--- src/pages/produkter.tsx
+++ src/pages/produkter.tsx
@@ -36,7 +36,7 @@ const Produkter: React.FC = () => {
     let filteredProducts = data;
 
     if (filters.price && filters.price.length > 0) {
-      filteredProducts = filteredProducts.filter(product => filters.price.includes(getPriceRange(product)));
+      filteredProducts = filteredProducts.filter(product => filters.price.includes(getPriceRange(product.pris, product.rabatt)));
     }
 
     if (filters.color && filters.color.length > 0) {
@@ -56,30 +56,30 @@ const Produkter: React.FC = () => {
     setFilteredData(filteredProducts);
   };
 
-  const getPriceRange = (price: ProduktData) => {
-    if (price.rabatt > 0) {
-      const discountedPrice = Math.round(price.pris * (1 - price.rabatt));
+  const getPriceRange = (price: number, rabatt: number) => {
+    if (rabatt > 0) {
+      const discountedPrice = Math.round(price * (1 - rabatt));
       if (discountedPrice <= 500) {
         return '0-500';
       } else if (discountedPrice > 500 && discountedPrice <= 1000) {
         return '500-1000';
       } else if (discountedPrice > 1000 && discountedPrice <= 1500) {
         return '1000-1500';
-      } else if (price.pris > 1500 && discountedPrice <= 2000) {
+      } else if (discountedPrice > 1500 && discountedPrice <= 2000) {
         return '1500-2000';
       } else if (discountedPrice > 2000 && discountedPrice <= 2500) {
         return '2000-2500';
       }
     } else {  
-    if (price.pris <= 500) {
+    if (price <= 500) {
       return '0-500';
-    } else if (price.pris > 500 && price.pris <= 1000) {
+    } else if (price > 500 && price <= 1000) {
       return '500-1000';
-    } else if (price.pris > 1000 && price.pris <= 1500) {
+    } else if (price > 1000 && price <= 1500) {
       return '1000-1500';
-    } else if (price.pris > 1500 && price.pris <= 2000) {
+    } else if (price > 1500 && price <= 2000) {
       return '1500-2000';
-    } else if (price.pris > 2000 && price.pris <= 2500) {
+    } else if (price > 2000 && price <= 2500) {
       return '2000-2500';
     }
   }};

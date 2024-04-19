84370887ff0680e460c5d19eb8912d417c7da7f6
Martin_T <Tangenmartin06@gmail.com>
2024-04-19 13:05:56 +0200

    dede

--- server/update_antall.php
+++ server/update_antall.php
@@ -1,8 +1,8 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
+header("Content-Type: application/json; charset=UTF-8");
 
 require_once('config.php');
 
